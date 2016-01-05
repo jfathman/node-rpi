6f24d2e11881872b0d050d56e7ced6049b6718e0
Jim Fathman <jim.fathman@gmail.com>
2016-01-05 09:40:39 -0600

    Put timer in class.

--- blink-leds-es6/app.js
+++ blink-leds-es6/app.js
@@ -15,45 +15,58 @@ var rateBlue = process.argv[2] || 1000;
 var rateRed  = process.argv[3] || 2000;
 var duration = process.argv[4] || 5000;
 
-const ON = 1;
+const ON  = 1;
 const OFF = 0;
 
 class Led {
-    constructor(gpioPin, ledName, rate) {
-        this.gpioPin = gpioPin;
-        this.ledName = ledName;
-        this.rate = rate;
-        this.device = new Gpio(gpioPin, 'out');
+    constructor(pin, name) {
+        this.name = name;
+        this.timer = null;
+        this.device = new Gpio(pin, 'out');
     }
 
-    getRate() {
-        return this.rate;
+    state() {
+        return this.device.readSync();
+    }
+
+    on() {
+        this.device.writeSync(ON);
+    }
+
+    off() {
+        this.device.writeSync(OFF);
     }
 
     toggle() {
-        this.device.writeSync(this.device.readSync() === OFF ? ON : OFF);
+        this.state() == ON ? this.off() : this.on();
+    }
+
+    start(rate) {
+        var _this = this;
+        this.timer = setInterval(function () {
+            _this.toggle();
+        }, rate);
+    }
+
+    stop() {
+        this.timer && clearInterval(this.timer);
+        this.timer = null;
     }
 
     shutdown() {
-        this.device.writeSync(OFF);
+        this.stop();
+        this.off();
         this.device.unexport();
     }
 }
 
-var ledBlue = new Led(17, 'blue', rateBlue);
-var ledRed  = new Led(22, 'red', rateRed);
+var ledBlue = new Led(17, 'blue');
+var ledRed  = new Led(22, 'red');
 
-var timerBlue = setInterval(function() {
-    ledBlue.toggle();
-}, ledBlue.getRate());
+ledBlue.start(rateBlue);
+ledRed.start(rateRed);
 
-var timerRed = setInterval(function() {
-    ledRed.toggle();
-}, ledRed.getRate());
- 
 setTimeout(function() {
-    clearInterval(timerBlue);
-    clearInterval(timerRed);
     ledBlue.shutdown();
     ledRed.shutdown();
 }, duration);
